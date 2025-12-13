73f105afcef3e23272dc0c4c0c4d07be2548c29e
tumelomolefemogale <tmogale554@gmail.com>
2025-12-13 10:22:51 +0200

    Create and retrieve all envelopes

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello Express!");
+app.use(express.json());
+
+let envelopes = [];
+let nextId = 0;
+
+app.post('/envelopes', (req, res) => {
+    const { title, budget } = req.body;
+
+    if (!title || !budget) {
+        return res.status(400).send("Title and budget are required.")
+    }
+
+    const newEnvelope = {id: nextId++, title, budget}
+    envelopes.push(newEnvelope)
+
+    res.status(201).json(newEnvelope);
+})
+
+app.get('/envelopes', (req, res) => {
+    res.json(envelopes);
 })
 
 app.listen(PORT, () => {
