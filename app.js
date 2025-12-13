e8ba8b321404eb4dbfcbd603ba58543effd9de80
tumelomolefemogale <tmogale554@gmail.com>
2025-12-13 14:55:54 +0200

    Retrieve specific envelopes

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const PORT = 3000;
 
 app.use(express.json());
 
+// Store envelopes
 let envelopes = [];
 let nextId = 0;
 
+// Create envelopes
 app.post('/envelopes', (req, res) => {
     const { title, budget } = req.body;
 
@@ -18,12 +20,25 @@ app.post('/envelopes', (req, res) => {
     envelopes.push(newEnvelope)
 
     res.status(201).json(newEnvelope);
-})
+});
 
+// Retrieve specific envelopes
+app.get('/envelopes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const envelope = envelopes.find(env => env.id === id);
+
+    if (!envelope) {
+        res.status(404).send("Envelope not found.")
+    }
+
+    res.json(envelope);
+});
+
+// Retrieve all envelopes
 app.get('/envelopes', (req, res) => {
     res.json(envelopes);
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}.`);
-})
+});
