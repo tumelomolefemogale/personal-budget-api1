7c8db048498efb391d3b393cfc00c92f557291dd
tumelomolefemogale <tmogale554@gmail.com>
2025-12-13 15:25:27 +0200

    Update specific envelopes

--- app.js
+++ app.js
@@ -13,31 +13,57 @@ app.post('/envelopes', (req, res) => {
     const { title, budget } = req.body;
 
     if (!title || !budget) {
-        return res.status(400).send("Title and budget are required.")
+        return res.status(400).send("Title and budget are required.");
     }
 
-    const newEnvelope = {id: nextId++, title, budget}
-    envelopes.push(newEnvelope)
+    const newEnvelope = {id: nextId++, title, budget};
+    envelopes.push(newEnvelope);
 
     res.status(201).json(newEnvelope);
-});
-
-// Retrieve specific envelopes
-app.get('/envelopes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const envelope = envelopes.find(env => env.id === id);
-
-    if (!envelope) {
-        res.status(404).send("Envelope not found.")
-    }
-
-    res.json(envelope);
 });
 
 // Retrieve all envelopes
 app.get('/envelopes', (req, res) => {
     res.json(envelopes);
 });
+
+// Retrieve specific envelopes
+app.get('/envelopes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const envelope = envelopes.find(env => env.id === id);
+
+    if (!envelope) {
+        res.status(404).send("Envelope not found.");
+    }
+
+    res.json(envelope);
+});
+
+// Update specific envelopes
+app.put('/envelopes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const envelope = envelopes.find(env => env.id === id);
+
+    if (!envelope) {
+        res.status(404).send("Envelope not found.");
+    }
+
+    const { title, withdrawal } = req.body;
+
+    if (title) {
+        envelope.title = title;
+    }
+
+    if (withdrawal) {
+        if (withdrawal > envelope.budget) {
+            res.status(400).send("You have insufficient funds.");
+        }
+
+        envelope.budget -= withdrawal
+    }
+
+    res.json(envelope)
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}.`);
