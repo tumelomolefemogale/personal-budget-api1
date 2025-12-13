45c4c3d19cb658d06abe6729fa5eeacdfcfb982d
tumelomolefemogale <tmogale554@gmail.com>
2025-12-13 19:19:49 +0200

    Transfer between envelopes

--- app.js
+++ app.js
@@ -27,6 +27,37 @@ app.get('/envelopes', (req, res) => {
     res.json(envelopes);
 });
 
+// Transfer from envelopes
+app.post('/envelopes/transfer/:from/:to', (req, res) => {
+    const fromId = Number(req.params.from);
+    const toId = Number(req.params.to);
+    const { amount } = req.body;
+
+    if (!amount || amount <= 0) {
+        return res.status(400).send("Transfer amount must be greater than 0.");
+    }
+
+    const fromEnvelope = envelopes.find(env => env.id === fromId);
+    const toEnvelope = envelopes.find(env => env.id === toId);
+
+    if (!fromEnvelope || !toEnvelope) {
+        return res.status(404).send("One or both envelopes not found.");
+    }
+
+    if (fromEnvelope.budget < amount) {
+        return res.status(400).send("Not enough budget to transfer.");
+    }
+
+    fromEnvelope.budget -= amount;
+    toEnvelope.budget += amount;
+
+    res.json({
+        message: "Transfer successful",
+        from: fromEnvelope,
+        to: toEnvelope
+    })
+});
+
 // Retrieve specific envelopes
 app.get('/envelopes/:id', (req, res) => {
     const id = Number(req.params.id);
@@ -77,7 +108,7 @@ app.delete('/envelopes/:id', (req, res) => {
     envelopes = envelopes.filter(env => env.id !== id);
 
     res.send(`Envelope with ID ${id} deleted successfully.`);
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}.`);
